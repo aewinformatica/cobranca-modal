bbeb2c280f95fd5d83bc000ae26585916a3b23ed
WAGNER <aewinformatica@gmail.com>
2019-10-07 09:31:37 -0300

    Incluindo classe para criaçao de propriedades

--- src/main/java/com/aewinformatica/cobranca/CobrancaApplication.java
+++ src/main/java/com/aewinformatica/cobranca/CobrancaApplication.java
@@ -2,21 +2,23 @@ package com.aewinformatica.cobranca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 
+import com.aewinformatica.cobranca.config.property.AewCobrancaProperty;
+
 @SpringBootApplication
+@EnableConfigurationProperties(AewCobrancaProperty.class)//para usar a configuração externamente pelo Application.properties
 public class CobrancaApplication {
 
 	private static ApplicationContext APPLICATION_CONTEXT;
 	
 	public static void main(String[] args) {
-		APPLICATION_CONTEXT = SpringApplication.run(CobrancaApplication.class, args);
-		
+		APPLICATION_CONTEXT = SpringApplication.run(CobrancaApplication.class, args);	
 	}
+	
 	public static <T> T getBean(Class<T> requiredType) {
 		return APPLICATION_CONTEXT.getBean(requiredType);
 	}
-	
-
 	
 }
